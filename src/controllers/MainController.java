670d46915a6db9e8653a96b4c5412bd70ed6efac
Philip Ekholm <philipekholm@protonmail.com>
2018-07-03 11:49:56 +0200

    Implemented ability to open images on disk and view them

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,12 +1,55 @@
 package controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import views.MainView;
 
 public class MainController{
 	private MainView mainWindow;
+	private BufferedImage currentImage;
 	
 	public MainController(Stage primaryStage) {
-		mainWindow = new MainView(primaryStage);
+		mainWindow = new MainView(primaryStage, this);
+	}
+	
+	public void loadImage() {
+		File inputFile;
+		
+		FileChooser imageChooser = new FileChooser();
+		imageChooser.setTitle("Open the image you want to edit");
+		inputFile = imageChooser.showOpenDialog(mainWindow.getPrimaryStage());
+		
+		System.out.println(inputFile.getAbsolutePath());
+		
+		try {
+			this.currentImage = ImageIO.read(inputFile);
+		}
+		catch(IOException e1){
+			e1.printStackTrace();
+		}
+		catch(IllegalArgumentException e2) {
+			e2.printStackTrace();
+		}
+		
+		refreshViewedImage();
+	}
+	
+	private void refreshViewedImage() {
+		mainWindow.setViewedImage(currentImage);
 	}
 }
+
+
+
+
+
+
+
+
+
--- src/views/MainView.java
+++ src/views/MainView.java
@@ -1,5 +1,10 @@
 package views;
 
+import java.awt.image.BufferedImage;
+import java.util.Calendar;
+
+import controllers.MainController;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -7,54 +12,61 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainView {
-	private VBox root;
-	private MenuBar menuBar;
+	private final VBox root;
+	private final Stage primaryStage;
+	private final MenuBar menuBar;
+	private final MainController controller;
+	private final Calendar cal = Calendar.getInstance();
+	private final ImageView imageView = new ImageView();
+	private Image shownImage = new Image("file:ui-images/default.jpg");
+	
+	final Menu file 	= new Menu("File");
+	final Menu filters 	= new Menu("Filters");
+	final Menu help     = new Menu("Help");
+	
+	final MenuItem open    = new MenuItem("Open...");
+	
+	final MenuItem close   = new MenuItem("Close");
+	final MenuItem save    = new MenuItem("Save");
+	final MenuItem saveAs  = new MenuItem("Save As...");
+	
+	final MenuItem aboutPanera = new MenuItem("About Panera...");
 
-	public MainView(Stage primaryStage) {
+	public MainView(Stage primaryStage, MainController controller) {
+		this.primaryStage = primaryStage;
+		this.controller = controller;
+
 		root = new VBox();
 		menuBar = new MenuBar();
 		Scene primaryScene = new Scene(root, 600, 400);
 		
-		primaryStage.setTitle("Panera");
+		imageView.setImage(shownImage);
+		imageView.setFitWidth(primaryScene.getWidth());
+		imageView.fitWidthProperty().bind(primaryScene.widthProperty());
+		imageView.fitHeightProperty().bind(primaryScene.heightProperty());
+		imageView.setPreserveRatio(true);
+
+		this.primaryStage.setTitle("Panera");
+		this.primaryStage.getIcons().add(new Image("file:ui-images/panera.png"));
+		this.primaryStage.setScene(primaryScene);
 		
-		primaryStage.setScene(primaryScene);
-		
-		primaryStage.show();
+		this.primaryStage.show();
 
 		root.getChildren().add(menuBar);
-		createMenu();
+		root.getChildren().add(imageView);
+		structureMenu();
+		setEventHandlers();
 	}
 	
-	private void createMenu() {
-		final Menu file 	= new Menu("File");
-		final Menu filters 	= new Menu("Filters");
-		final Menu help     = new Menu("Help");
-		
-		final MenuItem open    = new MenuItem("Open...");
-		final MenuItem close   = new MenuItem("Close");
-		final MenuItem save    = new MenuItem("Save");
-		final MenuItem saveAs  = new MenuItem("Save As...");
-		
-		final MenuItem aboutPanera = new MenuItem("About Panera...");
-		
-		aboutPanera.setOnAction(new EventHandler<ActionEvent> () {
-			@Override
-			public void handle(ActionEvent e) {
-				Alert a = new Alert(AlertType.INFORMATION);
-				a.setTitle("About Panera...");
-				a.setHeaderText("Panera v0.1");
-
-				a.setContentText("Written by Philip Ekholm, Copyright");
-				a.showAndWait();
-			}
-		});
-		
+	private void structureMenu() {
 		file.getItems().addAll(
 			open,
 			new SeparatorMenuItem(),
@@ -73,4 +85,39 @@ public class MainView {
 			help
 		);
 	}
+	
+	private void setEventHandlers() {
+		aboutPanera.setOnAction(new EventHandler<ActionEvent> () {
+			@Override
+			public void handle(ActionEvent e) {
+				Alert a = new Alert(AlertType.INFORMATION);
+				a.setTitle("About Panera...");
+				a.setHeaderText("Panera version 0.1");
+
+				a.setContentText("Written by Philip Ekholm, Copyright " + cal.get(Calendar.YEAR));
+				a.showAndWait();
+			}
+		});
+		
+		open.setOnAction(new EventHandler<ActionEvent> () {
+			@Override
+			public void handle(ActionEvent event) {
+				controller.loadImage();
+			}
+		});
+	}
+	
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	
+	public void setViewedImage(BufferedImage img) {
+		imageView.setImage(SwingFXUtils.toFXImage(img, null));
+	}
 }
+
+
+
+
+
+
